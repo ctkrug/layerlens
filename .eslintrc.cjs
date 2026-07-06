860876924f33bab8f8ac80210ea63ecf35ab3a4f
ctkrug <ctkrug4501@gmail.com>
2026-07-06 21:02:31 +0000

    build: add ESLint config for TypeScript

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+/* ESLint config for the TypeScript source. */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 2021, sourceType: 'module' },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  env: { browser: true, es2021: true, node: true },
+  ignorePatterns: ['dist/', 'node_modules/', '*.cjs'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'no-console': 'off',
+  },
+};
